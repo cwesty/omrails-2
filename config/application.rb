adfb6c1d8c0d0c274f63c452c78b1c23bd4a4497
Chris Westphal <c.westy@gmail.com>
2013-02-12 16:24:12 -0800

    Add user model with devise

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,16 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
+
+require 'devise/orm/active_record'
 
 module Omrails
   class Application < Rails::Application
@@ -58,5 +62,7 @@ module Omrails
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    # required for devise on heroku
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Omrails::Application.routes.draw do
   
+  devise_for :users
+
+  devise_for :installs
+
   get 'about' => 'pages#about'
 
   #root :to => ''
